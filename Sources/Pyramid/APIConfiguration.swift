76fa3f36184d6600bec3a1f434d856ccf5930e37
saroar <saroar9@gmail.com>
2020-10-07 22:29:36 +0300

    missing auth type

--- Sources/Pyramid/APIConfiguration.swift
+++ Sources/Pyramid/APIConfiguration.swift
@@ -12,7 +12,7 @@ public protocol APIConfiguration {
     var baseURL: URL { get }
     var method: HTTPMethod { get }
     var dataType: DataType { get }
-//    var authType: AuthType { get }
+    var authType: AuthType { get }
     var pathPrefix: String { get }
     var path: String { get }
     var contentType: ContentType? { get }
